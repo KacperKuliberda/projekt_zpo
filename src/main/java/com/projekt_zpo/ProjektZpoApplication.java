71b5a57ff1e556f293f9b5522b97ea57f128a0c1
KacperKuliberda <56536801+KacperKuliberda@users.noreply.github.com>
2019-11-06 14:10:53 +0100

    Initial Commit

---
+++ src/main/java/com/projekt_zpo/ProjektZpoApplication.java
@@ -0,0 +1,13 @@
+package com.projekt_zpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektZpoApplication.class, args);
+	}
+
+}
